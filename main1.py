373bb6578742e5da9b6f0b054c9c3da5a879c727
madhavi-ch <madhavichimata99@gmail.com>
2021-11-08 17:18:24 +0530

    comment

---
+++ main1.py
@@ -0,0 +1,8 @@
+'''program to check whether given number is perfect square or not'''
+import math
+n=int(input("enter number"))
+root=math.sqrt(n)
+if int(root+0.5)**2==n:
+	print(n "is perfect square")
+else:
+	print(n "is not perfect square")
